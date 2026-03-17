cf23c4bc9b6844b4b61066df7d6bf36543d4db46
Melinda Tran <meltran@bu.edu>
2026-03-16 20:06:15 -0400

    service files

---
+++ services/elevator_service.py
@@ -0,0 +1,13 @@
+# elevator_service.py handles:
+#updates elevator states
+# applies safety protocols
+# coordinates between requests and elevators
+
+from models.elevator import ElevatorState
+
+def update_elevator_state(state: ElevatorState) -> None:
+    """update elevator state  """
+    pass
+def process_next_stop(elevator_id: str) -> None:
+    """advance elevator to next scheduled stop """
+    pass
---
+++ services/scheduler.py
@@ -0,0 +1,14 @@
+# scheduler.py handles
+# chooses which elevator handles a request
+#can evolve from simple to complex algorithms
+
+
+from models.request import ElevatorRequest
+
+def assign_elevator(request: ElevatorRequest) -> str:
+    """
+    determine which elevator should handle a request
+    returns:
+        elevator_id(str)
+    """
+    pass
